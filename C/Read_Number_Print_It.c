e1666beb2f8a67f207374b209fde223d68dae1a4
Manoj Kumar <manoj.jkit91@gmail.com>
2019-06-15 13:12:35 +0530

    Take An Integer As Input and print it

---
+++ C/Read_Number_Print_It.c
@@ -0,0 +1,12 @@
+/*
+ * Take An Integer As Input and print it
+ */
+#include <stdio.h>
+int main ( int argc, char **argv )
+{
+  int num;
+  printf("Enter An Integer\n");
+  scanf("%d", &num);
+  printf("You Have Entered Integer %d \n", num);
+  return 0;
+}
